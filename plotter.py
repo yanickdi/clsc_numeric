a3e2e14962c9d10486ef9ca074fd951f51f52a0f
yanickd <yanick@dickbauer.at>
2017-06-29 10:31:47 +0200

    zwischenst

--- plotter.py
+++ plotter.py
@@ -1,7 +1,9 @@
 import argparse
 from math import log
+import sys
 
 import numpy as np
+import matplotlib as mp
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
@@ -12,6 +14,7 @@ PLOT_PROFIT_DIFFERENCE = 'profit-difference'
 PLOT_RHO_DIFFERENCE = 'rho-difference'
 PLOT_CASES_MODEL_ONE = 'cases-model-one'
 PLOT_CASES_MODEL_TWO = 'cases-model-two'
+AUTOMATED_PLOTS = 'automated-plots'
 
 _ALL_CASES_MODEL_1 = [solver._CASE_ONE, solver._CASE_TWO]
 _ALL_CASES_MODEL_2 = [solver._CASE_ONE_A, solver._CASE_ONE_B, solver._CASE_ONE_C,
@@ -31,6 +34,7 @@ class CountourPlotter:
         }[type]
         self.absolute = params['absolute']
         self.gray = params['gray']
+        self.output = params['output']
         if type == PLOT_PROFIT_DIFFERENCE and self.absolute: self._calc_func = self.__profit_calc_func_absolute
         elif type == PLOT_RHO_DIFFERENCE  and self.absolute: self._calc_func = self.__rho_calc_func_absolute
         self.matrix = None
@@ -41,11 +45,22 @@ class CountourPlotter:
             it varies cn from [cr, 1] and a from [0, upper_bound_a]
             Returns a tuple of (par_model_1, par_model_2)
         """
-        #assert self.lower_bound_cn >= self.cr
+        def __cr(cn):
+            if self.cr == 'delta*cn/2':
+                return self.delta * cn / 2
+            else:
+                return self.cr
+                
+        def __s(cn):
+            if self.s == 'cn/2':
+                return cn / 2
+            else:
+                return self.s
+                
         for line, cn in enumerate(drange(self.lower_bound_cn, self.upper_bound_cn, self.step_size_cn)):
             for col, a in enumerate(drange(self.lower_bound_a, self.upper_bound_a, self.step_size_a)):
-                par_model_1 = Parameter(MODEL_1, tau=self.tau, a=a, s=self.s, cn=cn)
-                par_model_2 = Parameter(MODEL_2, tau=self.tau, a=a, s=self.s,  cr=self.cr, cn=cn, delta=self.delta)
+                par_model_1 = Parameter(MODEL_1, tau=self.tau, a=a, s=__s(cn), cn=cn)
+                par_model_2 = Parameter(MODEL_2, tau=self.tau, a=a, s=__s(cn),  cr=__cr(cn), cn=cn, delta=self.delta)
                 yield (line, col, par_model_1, par_model_2)
     
     def calc(self):
@@ -63,7 +78,6 @@ class CountourPlotter:
                 sol_model_2 = solver_m2.optimize(par_model_2)
             self.matrix[line, col] = self._calc_func(sol_model_1, sol_model_2, par_model_2)
             i += 1
-        print(i)
         
     def __rho_calc_func(self, sol_model_1, sol_model_2, par):
         if sol_model_1 is None or sol_model_2 is None:
@@ -76,10 +90,10 @@ class CountourPlotter:
         return (sol_model_2.dec.rho - sol_model_1.dec.rho)
         
     def __profit_calc_func(self, sol_model_1, sol_model_2, par):
-        if sol_model_1 is None or sol_model_2 is None or sol_model_1.profit_man < 0.01:
+        if sol_model_1 is None or sol_model_2 is None:# or sol_model_1.profit_man < 0.01:
             return np.nan
         rel =  (sol_model_2.profit_man - sol_model_1.profit_man) / sol_model_1.profit_man
-        return rel
+        return max(0, min(1, rel))
         
     def __profit_calc_func_absolute(self, sol_model_1, sol_model_2, par):
         if sol_model_1 is None or sol_model_2 is None:
@@ -98,27 +112,6 @@ class CountourPlotter:
             return np.nan
         else:
             return _ALL_CASES_MODEL_2.index(sol_model_2.case) + 1
-            
-    def __plot_cases(self):
-        """ not used any more"""
-        if self.type == PLOT_CASES_MODEL_ONE:
-            title = r'Which case will be active in the event of no Online Shop'
-            side_title = r'0 = no solution, 1 = case 1,   2 = case 2'
-        elif self.type == PLOT_CASES_MODEL_TWO:
-            title = r'Which case will be active in the event of having an Online Shop'
-            side_title = r'0=no sol, 1=c1a, 2=c1b, 3=c1c, 4=c2a, 5=c2b, 6=c3b'
-            
-        fig = plt.figure()
-        ax = plt.subplot(111)
-        
-        imgplot = ax.matshow(self.matrix)
-        fig.suptitle(title)
-        cbar = plt.colorbar(imgplot)
-        # Put a text of paramaters below current axis
-        txt = r'par: $\tau$={:.2f}, s={:.2f}, cr={:.2f}, $\delta$={:.2f}'.format(self.tau, self.s, self.cr, self.delta)
-        fig.text(0.6, 0.05, txt, fontsize=8, bbox=dict(facecolor='white', alpha=0.5))
-        plt.subplots_adjust(bottom=0.15)
-        plt.show()
         
     def plot_contourf(self):
         if self.type == PLOT_PROFIT_DIFFERENCE:
@@ -136,10 +129,10 @@ class CountourPlotter:
         elif self.type == PLOT_CASES_MODEL_ONE:
             title = r'Which case will be active in the event of no Online Shop'
             side_title = r'0 = no solution, 1 = case 1,   2 = case 2'
-            cbar_ticks = range(2)
+            cbar_ticks = None
         elif self.type == PLOT_CASES_MODEL_TWO:
             title = r'Which case will be active in the event of having an Online Shop'
-            side_title = r'0=no sol, 1=c1a, 2=c1b, 3=c1c, 4=c2a, 5=c2b, 6=c3b'
+            side_title = r'1=c1a, 2=c1b, 3=c1c, 4=c2a, 5=c2b, 6=c3b'
             cbar_ticks = range(2,7)
         cmap = cm.gray if self.gray else None            
         fig = plt.figure()
@@ -147,19 +140,44 @@ class CountourPlotter:
         
         x_vector = np.linspace(self.lower_bound_a, self.upper_bound_a, num=self.nr_cols)       # x is a
         y_vector = np.linspace(self.lower_bound_cn, self.upper_bound_cn, num=self.nr_lines)    # y is cn
-        cont = ax.contourf(x_vector, y_vector, self.matrix, label='heatmap', cmap=cmap)
+        
+        #levels = [0, 1, 100]
+        #colors = ['green', 'blue', 'yellow', 'black']
+        #extend = 'both'
+        #cmap, norm = mp.colors.from_levels_and_colors(levels, colors, extend=extend)
+        #m = plt.pcolormesh(self.matrix, cmap=cmap, norm=norm)
+        #plt.colorbar(m)
+        #plt.show()
+        #sys.exit()
+ 
+        #cont = ax.contourf(x_vector, y_vector, self.matrix, label='heatmap', cmap=cmap, norm=norm)
+        cont = ax.contourf(x_vector, y_vector, self.matrix, label='heatmap')
         fig.suptitle(title)
         ax.set_xlabel('a')
         ax.set_ylabel(r'$c_n$')
         
-        cbar = plt.colorbar(cont, ticks=cbar_ticks)
+        cbar = plt.colorbar(cont)#, ticks=cbar_ticks)
         cbar.ax.set_ylabel(side_title)
         
         # Put a text of paramaters below current axis
-        txt = r'par: $\tau$={:.2f}, s={:.2f}, cr={:.2f}, $\delta$={:.2f}'.format(self.tau, self.s, self.cr, self.delta)
+        txt = self.__par_txt()
         fig.text(0.6, 0.05, txt, fontsize=8, bbox=dict(facecolor='white', alpha=0.5))
         plt.subplots_adjust(bottom=0.15)
-        plt.show()
+        if self.output == None: plt.show()
+        else:
+            plt.savefig(self.output)
+        plt.close(fig)
+            
+    def __par_txt(self):
+        if self.cr == 'delta*cn/2':
+            _cr = r'$\frac{\delta*cn}{2}$'
+        else:
+            _cr = '{:.2f}'.format(self.cr)
+        if self.s == 'cn/2':
+            _s = r'$\frac{1}{2}cn$'
+        else:
+            _s = '{:.2f}'.format(self.s)
+        return r'par: $\tau$={:.2f}, s={}, cr={}, $\delta$={:.2f}'.format(self.tau, _s, _cr, self.delta)
         
     def plot(self):
         self.plot_contourf()
@@ -169,19 +187,73 @@ def __parser_output_file(string):
         return string
     if len(string.split('.')) <= 1:
         raise argparse.ArgumentTypeError('The output file has to have a suffix.')
+    allowed_suffixes = ('png', 'pdf')
     suffix = string.split('.')[-1]
-    if suffix not in ('png'):
-        raise argparse.ArgumentTypeError('Supported output types are: .png')
+    if suffix not in allowed_suffixes:
+        raise argparse.ArgumentTypeError('Supported output types are: .png or .pdf')
     else:
-        for all in allowed:
+        for all in allowed_suffixes:
             if all == string: return all
+    return string
     
 def __parser_plot_name(string):
-    allowed = (PLOT_PROFIT_DIFFERENCE, PLOT_RHO_DIFFERENCE, PLOT_CASES_MODEL_ONE, PLOT_CASES_MODEL_TWO)
+    allowed = (PLOT_PROFIT_DIFFERENCE, PLOT_RHO_DIFFERENCE, PLOT_CASES_MODEL_ONE, PLOT_CASES_MODEL_TWO, AUTOMATED_PLOTS)
     if string not in allowed:
         raise argparse.ArgumentTypeError('Implemented plots are: ' + ' or '.join(allowed))
     return string
+    
 
+def __gen_parameter_automated_plots():
+    def __gen():
+        for plot in ('profit-difference', 'rho-difference', 'cases-model-one', 'cases-model-two'):
+            for absolute in (True, False):
+                for delta in (.4, .8):
+                    for s in (0, 'cn/2'):
+                        for tau in (.02, .05):
+                            for cr in (.1, 'delta*cn/2'):
+                                yield {'plot' : plot, 'absolute' : absolute, 'delta' : delta, 's' : s, 'tau' : tau, 'cr' : cr}
+    for elem in __gen():
+        if elem['plot'] in ('cases-model-one', 'cases-model-two') and elem['absolute'] == True:
+            pass
+        else:
+            yield elem
+
+def automated_plots(step_size_a, step_size_cn, gray):
+    for nr, parms in enumerate(__gen_parameter_automated_plots()):
+        filename = _automated_plots_filename(nr, parms)
+        plotter = CountourPlotter(parms['plot'], params={
+            'tau': parms['tau'], 's': parms['s'], 'cr': parms['cr'], 'delta' : parms['delta'],
+            'step_size_a' : step_size_a, 'lower_bound_a' : .0, 'upper_bound_a' : .04,
+            'step_size_cn' : step_size_cn, 'lower_bound_cn' : .0, 'upper_bound_cn' : 1.0,
+            'absolute' : parms['absolute'],
+            'gray'   : gray,
+            'output' : filename
+        })
+        print('plotting {} ... '.format(filename), end='')
+        sys.stdout.flush()
+        plotter.calc()
+        plotter.plot()
+        print('done')
+        sys.stdout.flush()
+        
+def _automated_plots_filename(nr, parms):
+    plot_name = parms['plot']
+    if plot_name in ('profit-difference', 'rho-difference'):
+        plot_name += '_abs_' if parms['absolute'] else '_rel_'
+        
+    par_keys = ['delta', 's', 'tau', 'cr']
+    par_strings = []
+    for key in par_keys:
+        if type(parms[key]) == str:
+            str_expr = parms[key].replace('/', '_over_').replace('*', '_times_')
+            par_strings.append('{}_{}'.format(key, str_expr))
+        else:
+            par_strings.append('{}_{:.3f}'.format(key, parms[key])) #par should be a float
+    par_string = '_'.join(par_strings)
+    filename = 'plot_{}_{}_{}.pdf'.format(nr+1, plot_name, par_string)
+    return filename
+    
+    
 if __name__ == '__main__':
     # parse the command line
     parser = argparse.ArgumentParser(description='Plotting module of clsc solver')
@@ -193,22 +265,32 @@ if __name__ == '__main__':
     args = parser.parse_args()
     
     quality = 'low' if args.low_qual else 'high'
-    absolute = args.absolute
-    gray = args.gray
-    
     if quality == 'high':
         step_size_a = .0001
         step_size_cn = .001
     elif quality == 'low':
         step_size_a = .001
         step_size_cn = .01
+    gray = args.gray
     
-    plotter = CountourPlotter(args.plot[0], params={
-        'tau': .3, 's': .07, 'cr': .1, 'delta' : .3,
-        'step_size_a' : step_size_a, 'lower_bound_a' : .0, 'upper_bound_a' : .04,
-        'step_size_cn' : step_size_cn, 'lower_bound_cn' : .0, 'upper_bound_cn' : .7,
-        'absolute' : absolute,
-        'gray' : gray
-    })
-    plotter.calc()
-    plotter.plot()
+    plot = args.plot[0]
+    if plot == AUTOMATED_PLOTS:
+        automated_plots(step_size_a, step_size_cn, gray)
+    else:
+        absolute = args.absolute
+        if args.output:
+            output = args.output[0]
+        else:
+            output = None
+        
+        
+        plotter = CountourPlotter(args.plot[0], params={
+            'tau': .3, 's': .07, 'cr': .1, 'delta' : .3,
+            'step_size_a' : step_size_a, 'lower_bound_a' : .0, 'upper_bound_a' : .04,
+            'step_size_cn' : step_size_cn, 'lower_bound_cn' : .0, 'upper_bound_cn' : 1.,
+            'absolute' : absolute,
+            'gray'   : gray,
+            'output' : output
+        })
+        plotter.calc()
+        plotter.plot()
